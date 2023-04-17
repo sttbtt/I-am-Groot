b7af042690d1764055dd5ed49188e9b041829bef
Scott Bennett <stt.btt@gmail.com>
2023-04-17 19:07:21 -0400

    Initial Commit

---
+++ I am Groot/I_am_GrootApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_GrootApp.swift
+//  I am Groot
+//
+//  Created by Scott Bennett on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct I_am_GrootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
